c0875f5c32d49334a530f9138eabceb635f1ad3a
Rob <alekseicruz@gmail.om>
2020-04-06 21:48:19 -0700

    Initial checkin

--- example.js
+++ example.js
@@ -2,6 +2,7 @@ var x_num_1 =  parseInt(prompt('Give me a number!'))
 var num_1 =  parseInt(prompt('Give me a number!'))
 var num_2 = parseInt(prompt("Give me another number!"))
 var result;
+var result_1;
 
 var operation = prompt('What would you like to do? (add, subtract, multiply, divide)').toUpperCase();
 
